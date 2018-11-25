19a0d42d409dd861d28df3348227cf951e18f883
root <root@localhost.localdomain>
2018-11-26 01:20:56 +0900

    make it better

--- application.py
+++ application.py
@@ -70,7 +70,7 @@ def handle_message(event):
         
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=messages)) # messagesに代入されている値を返してくれる
+        TextSendMessage(text="messages")) # messagesに代入されている値を返してくれる
  
 # ポート番号の設定
 if __name__ == "__main__":

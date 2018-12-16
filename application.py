3cea373ac9862ae8744dec3a1be025b40fb4d23a
root <root@localhost.localdomain>
2018-12-16 23:41:43 +0900

    comment

--- application.py
+++ application.py
@@ -107,8 +107,9 @@ def handle_message(event):
             if row["status"] == 0:
                 df.loc[index, 'status'] = 1
                 # rentaluserに代入する値にはLINEIDを入れる
-                user_id = event.source.user_id
-                df.loc[index, 'rentaluser'] = user_id
+                user_disp_name = profile.display_name
+                #user_id = event.source.user_id
+                df.loc[index, 'rentaluser'] = user_disp_name
                 messages = "借りれるよ"
                 break
             else:

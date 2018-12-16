c240e673f84e328fc70fc9eed13aa6c5bd85a07a
root <root@localhost.localdomain>
2018-12-17 01:39:28 +0900

    comment

--- application.py
+++ application.py
@@ -102,6 +102,7 @@ def handle_message(event):
             #userstatus = 0
             #se = pd.Series([user_id,user_disp_name,0],index=df1.columns)
             df3 = df1.append(df2, ignore_index=True)
+            df3 = df3.drop(["Unnamed: 0"],axis=1)
             df3.to_csv(file_name1,encoding="shift_jis")
             service.create_blob_from_path(container_name,file_name1,file_name1)
             messages = "登録しました"

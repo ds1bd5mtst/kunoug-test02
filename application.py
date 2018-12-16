7939940c10e2c1c1385e076141cb138e528c4943
root <root@localhost.localdomain>
2018-12-17 01:15:16 +0900

    comment

--- application.py
+++ application.py
@@ -96,12 +96,12 @@ def handle_message(event):
             messages = "登録済"
             break
         else:
-            #df1 = pd.DataFrame(columns=['LINEID','username','userstatus'])
+            df1 = pd.DataFrame(columns=['LINEID','username','userstatus'])
             #LINEID = user_id
             #username = user_disp_name
             #userstatus = 0
             se = pd.Series([user_id,user_disp_name,0],index=df1.columns)
-            df1 = df1.append(se, ignore_index=True)
+            df1.append(se, ignore_index=True)
             df1.to_csv(file_name1,encoding="shift_jis")
             service.create_blob_from_path(container_name,file_name1,file_name1)
             messages = "登録しました"

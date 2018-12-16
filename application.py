8c6751d161b805dc52c7de82eba5f995c2bb8943
root <root@localhost.localdomain>
2018-12-17 00:58:36 +0900

    comment

--- application.py
+++ application.py
@@ -89,7 +89,7 @@ def handle_message(event):
     
     messages = "test"
     
-    
+    """
     messages = ""
     for index, row in df1.iterrows():
         if row["LINEID"] ==  user_id :
@@ -105,7 +105,7 @@ def handle_message(event):
             df1.to_csv(file_name1,encoding="shift_jis")
             service.create_blob_from_path(container_name,file_name1,file_name1)
             messages = "登録しました"
-    
+    """
     
     """
     # CSV読み込み

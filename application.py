19ec5e8c34f0ab03ddf1ce7f0f7ac6d9112b579e
root <root@localhost.localdomain>
2018-12-17 18:55:37 +0900

    comment

--- application.py
+++ application.py
@@ -83,9 +83,11 @@ def handle_message(event):
     service.get_blob_to_path(container_name,file_name,file_name)
     service.get_blob_to_path(container_name,file_name1,file_name1)
     
+    messages = "DLできたよ"
+    
     df1 = pd.read_csv(file_name1,encoding="utf-8", sep=",")
     
-    messages = "読み込めたよ"
+    #messages = "読み込めたよ"
     
     profile = line_bot_api.get_profile(event.source.user_id)
     user_disp_name = profile.display_name

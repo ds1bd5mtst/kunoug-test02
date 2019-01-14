241d89b1af5a3ef667b24dad20e6ddacc9a128fc
root <root@localhost.localdomain>
2019-01-14 23:38:24 +0900

    comment

--- application.py
+++ application.py
@@ -83,7 +83,8 @@ def handle_message(event):
     service.get_blob_to_path(container_name,file_name,file_name)
     service.get_blob_to_path(container_name,file_name1,file_name1)
     
-    df1 = pd.read_csv(file_name1,encoding="shift_jis", sep=",")
+    df1 = pd.read_csv(file_name1,encoding="utf-8", sep=",")
+    #df1 = pd.read_csv(file_name1,encoding="shift_jis", sep=",")
     messages = "yometa"
     
     """
